d535b7b9327e70a0edf2a4f7dd6a7134b4aaf30b
ibrahimayuba393 <ibrahimayuba393@gmail.com>
2022-09-15 16:40:32 +0100

    added a few touches

--- src/components/home-page/home-components/hero-section.component.jsx
+++ src/components/home-page/home-components/hero-section.component.jsx
@@ -3,8 +3,8 @@ const HeroSection = () => {
     <div className="px-6 md:px-10">
       <div className="bg-zinc-200 rounded-2xl my-10 shadow-xl">
         <div className="grid grid-cols-2 items-center md:gap-12 p-4">
-          <h1 className="text-lg md:text-2xl font-bold pr-4">
-            Get top-quality kitchen amenities
+          <h1 className="text-lg text-center md:text-2xl font-bold">
+            Enhance your space with the perfect kitchen centerpiece
           </h1>
           <img
             className="w-full"
